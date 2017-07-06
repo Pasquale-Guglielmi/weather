6f0f2b9dfe54a69816c750586ebe5a6e07916773
Pasquale Guglielmi <guglielmipasquale@yahoo.it>
2017-07-06 16:21:59 +0200

    finish: UI for /forecast, showing fetched data

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -2,11 +2,24 @@ var React = require('react');
 var queryString = require('query-string');
 var api = require('../utils/api');
 
+
+function DayItem(props) {
+    var iconId = props.data.weather[0].icon;
+    var iconURL = '/app/icons/' + iconId + '.svg'
+    var date = new Date(props.data.dt*1000);
+    return (
+        <div className="day-item column">
+            <img src={iconURL} className="icon" />
+            <h2 className="day-header">{date.toDateString()}</h2>
+        </div>
+    )
+}
+
 class Forecast extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: false,
+            loading: true,
             results: {}
         };
     }
@@ -17,8 +30,9 @@ class Forecast extends React.Component {
                 loading: true
             }
         })
-        var query = queryString.parse(this.props.location.search).city;
-        this.fetchForecast(query);
+        this.city = queryString.parse(this.props.location.search).city;
+        this.fetchForecast(this.city);
+
     }
 
     componentWillReceiveProps() {
@@ -27,12 +41,12 @@ class Forecast extends React.Component {
                 loading: true
             }
         })
-        var query = queryString.parse(this.props.location.search).city;
-        this.fetchForecast(query);
+        this.city = queryString.parse(this.props.location.search).city;
+        this.fetchForecast(this.city);
     }
 
     fetchForecast(queryString) {
-        api.getWeather(queryString)
+        api.getForecast(queryString)
             .then((results) => {
                 this.setState(function() {
                     return {
@@ -40,14 +54,22 @@ class Forecast extends React.Component {
                         results: results
                     };
                 })
-                console.log(this.state);
             })
     }
 
     render() {
         return (
             <div>
-            {(this.state.loading) ? <div className="loading">LOADING</div> : <div>NOT LOADING</div>}
+            {(this.state.loading)
+                ? <div className="loading">LOADING</div>
+                : <div>
+                    <h2 className="forecast-header">{this.city}</h2>
+                    <div className="forecast-container">
+                        {this.state.results.map(function(dataObj) {
+                            return <DayItem key={dataObj.dt} data={dataObj} />
+                        }, this)}
+                    </div>
+                </div>}
             </div>
         )
     }
--- app/utils/api.js
+++ app/utils/api.js
@@ -2,45 +2,16 @@ var axios = require('axios');
 
 
 var key = '7976b116717344282e42d04344afd651';
-var currentWeatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=CITY-NAME&type=accurate&APPID=' + key;
 var forecastEndpoint ='http://api.openweathermap.org/data/2.5/forecast/daily?q=CITY-NAME&type=accurate&APPID=' + key + '&cnt=5';
-
-function iconURL (iconId) {
-    var iconEndpoing = 'http://openweathermap.org/img/w/ICON-ID.png';
-    return iconEndpoing.replace(/ICON-ID/, iconId);
-}
 
 function getForecast (location) {
     var query = forecastEndpoint.replace(/CITY-NAME/, location);
     return axios.get(query)
         .then(function(result) {
-            return result;
+            return result.data.list;
         })
 }
-
-function getCurrentWeather (location) {
-    var query = currentWeatherEndpoint.replace(/CITY-NAME/, location);
-    return axios.get(query)
-        .then(function(result) {
-            var weather = result.data.weather[0].description;
-            var icon = iconURL(result.data.weather[0].icon);
-            var temp = result.data.main.temp;
-            var hum = result.data.main.humidity;
-            return [weather,icon, 'Temperature: ' + temp + '˚K', 'Humidity: ' + hum + '%'];
-        })
-}
-
 
 module.exports = {
-    getWeather: function(location) {
-        return axios.all([
-            getCurrentWeather(location),
-            getForecast(location)
-        ]).then(function(data) {
-            return ({
-                currentWeather: data[0],
-                forecast: data[1]
-            })
-        })
-    }
+    getForecast
 }
